3079b11944cb10148f2d91b687c4c5c8055cc18e
Kasutaja <shr4528@gmail.com>
2022-07-28 18:37:21 +0300

    test

---
+++ 02_lesson/26.07.py
@@ -0,0 +1,56 @@
+# isikukood = input('Please enter national ID:')
+# # if len(isikukood) == 11:
+# #     print('your ID:' + isikukood)
+# # elif len(isikukood) > 11:
+# #     print('your id is too long')
+# # else:
+# #     print('your id is too short')
+# #
+# # if isikukood:
+# #    if len(isikukood) == 11:
+# #       print('your id:' + isikukood)
+# #   else:
+# #     if len(isikukood) > 11:
+# #         print('your id is too long')
+# #      else:
+# #         print('your id is too short')
+# # else:
+# #      print('no id entered:')
+
+#----------HomeWork--------------
+#write a code to return "Hello" from given string
+#example_string1 = "Hello bro"
+#print(example_string1[:2])
+
+#write a code to return "Jack is my name"
+#example_string2 = "jack is My NAME"
+#print(example_string2.capitalize())
+
+
+#write a code to return "Get rid of stars please"
+#example_string3 = "*Get rid of stars please*"
+#print(example_string3.strip('*'))
+
+#write a code to return "Hello my name Jack"
+#example_string4 = "hello my name jack"
+#print(example_string4.capitalize().replace('j','J'))
+
+
+
+#write a code to return formated string "Hello,my name is Jack"
+#var1 = "jack"
+#var2 = "hello"
+#var3 = "MY NAME IS"
+#print(f'{var2}')
+#test
+#write a code to return True if cost is greater than 500$
+#example_string5 = "it cost me 1000.00$"
+#cost = 1000
+#if cost > 500:
+#    print(True)
+#else:
+#   (False)
+#byte_string = b"\316\273"
+#print(byte_string.decode('utf-16'))
+
+
