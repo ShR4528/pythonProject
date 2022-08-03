bd5f83a96da78cc11ccf5e40994b9169978d5246
Kasutaja <shr4528@gmail.com>
2022-08-03 12:17:51 +0300

    test

---
+++ 03_lesson/05-lesson/028.py
@@ -0,0 +1,55 @@
+#------4-------
+
+#list = ['Hello', 'World', 'Print','Math','Auto']
+#list.reverse()
+#print(list)
+#-------4--------
+
+
+#--------5----------
+#numbers = [1, 2, 3, 5, 8]
+#numbers[2] = 8, 2, 5
+#print(numbers)
+#--------5-----------
+#if __name__ == '__main__':
+   # num1 = [1, 2, 3, 4, 7, 9, 9]
+  #  num2 = [1, 2, 4, 6, 4, 6]
+
+ #   dup = {x for x in num1 if num1.count(x) > 1}
+ #   dup2 = {x for x in num2 if num2.count(x) > 1}
+#print(dup)
+#print(dup2)
+
+#--------2------------
+#a = input('a = ')
+#b = input('b = ')
+#a = float(a)
+#b = float(b)
+#area = (a * b) / 2
+#c = (a**2 + b**2) ** 0.5
+#perimeter = a + b + c
+#print('Area:', area)
+#print('Perimeter:', perimeter)
+#---------2----------
+
+
+
+#-------1---------
+#name = 'Shamil'
+#surname = 'Ramazanov'
+#age = 33
+#print(f'Hello,{name} {surname}.Your age is:{age}')
+#--------1-----------
+
+#--------7-----------
+#time = float(input('Input Time: '))
+#day = time //(24 * 3600)
+#time = time % (24 * 3600)
+#hour = time // 3600
+#time %= 3600
+#minutes = time // 60
+#time %= 60
+#seconds = time
+#print('d:h:m:s -> %d:%d:%d:%d' % (day, hour, minutes, seconds))
+#----------7----------
+
